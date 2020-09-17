3fb196cd61d17f74453dcea903537dbdfc6db63f
srub <shani.rubovitch@gmail.com>
2020-09-17 11:32:37 +0200

    now establishes access point

--- Joystick_module/esp32_version/src/main.cpp
+++ Joystick_module/esp32_version/src/main.cpp
@@ -1,43 +1,40 @@
 #include <Arduino.h>
 #include <driver/adc.h>
-#include <ESP32AnalogRead.h>
-// ESP32AnalogRead adc;
+#include <HTTPClient.h>
+#include <WiFi.h>
 
 #define j_sw 33   // ADC1 5
-adc1_channel_t adc_j_sw = ADC1_CHANNEL_5;
 #define j_y 32    // ADC1 4
-adc1_channel_t adc_j_y = ADC1_CHANNEL_4;
 #define j_x 35    // ADC1 7
-adc1_channel_t adc_j_x = ADC1_CHANNEL_7;
 #define led 22
+
+const char* ssid     = "Olafchen";
+const char* password = "lalalalala";
 
 int x_value = 0;
 int y_value = 0;
 int sw_value = 0;
 
+
 void setup() {
+
   Serial.begin(9600);
+
+  // piModes
   pinMode(led, OUTPUT);
   pinMode(j_sw, INPUT_PULLUP);
   pinMode(j_y, ANALOG);
   pinMode(j_x, ANALOG);
+
+  // establish access point
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
 }
 
 void loop() {
-
-  /*
-  adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(adc_j_x, ADC_ATTEN_MAX);
-  adc1_config_channel_atten(adc_j_y, ADC_ATTEN_MAX);
-  adc1_config_channel_atten(adc_j_sw, ADC_ATTEN_MAX);
-  
-  x_value = adc1_get_raw(adc_j_x);
-  y_value = adc1_get_raw(adc_j_y);
-  sw_value = adc1_get_raw(adc_j_sw);
-  */
-
-  // adc.attach(j_x);
-  // x_value = adc.readVoltage();
   x_value = analogRead(j_x);
   y_value = analogRead(j_y);
   sw_value = digitalRead(j_sw);
